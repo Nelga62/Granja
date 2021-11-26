0b2d504edc754590cf855c67cb57576390265c2b
Javier <javiercp69@educastur.es>
2021-11-26 13:48:16 +0100

    C

--- src/granja/Granja.java
+++ src/granja/Granja.java
@@ -9,7 +9,7 @@ public class Granja
     
     public Granja(Granjero granjero)
     {
-        this.granjero = granjero; //AAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        this.granjero = granjero; //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAgtwtt
     }
     
     public Granja(String nombre, String ubicacion, Granjero granjero, int numeroAnimales)
